984521f4feba89ae0e11051e1c6d38751e8568f3
narinn-star <skfls2618@naver.com>
2024-04-08 23:48:27 +0900

    BOJ_[13904]_24.04.08

---
+++ BOJ13904 과제/BOJ13904.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ13904 {
+    static class Task {
+        int deadline;
+        int point;
+
+        Task(int deadline, int point) {
+            this.deadline = deadline;
+            this.point = point;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<Task> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int deadline = Integer.parseInt(st.nextToken());
+            int point = Integer.parseInt(st.nextToken());
+            list.add(new Task(deadline, point));
+        }
+
+        Collections.sort(list, (ol, o2) -> ol.deadline - o2.deadline);
+
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>();
+
+        for(int i = 0; i < list.size(); i++){
+            pQueue.add(list.get(i).point);
+            if(pQueue.size() > list.get(i).deadline){
+                pQueue.poll();
+            }
+        }
+
+        int res = 0;
+        while(!pQueue.isEmpty()){
+            res += pQueue.poll();
+        }
+
+        System.out.println(res);
+    }
+}
