0a5a6bb0e9aafbad7404878f347766111fd68762
narinn-star <skfls2618@naver.com>
2024-07-01 23:04:34 +0900

    BOJ_[15655]_24.07.01

---
+++ BOJ15655 N과 M (6)/BOJ15655.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ15655 {
+    static int N, M;
+    static int[] inputArr, numbers;
+    static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[M];
+        inputArr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inputArr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(inputArr);
+
+        dfs(0, 0);
+
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int depth, int root) throws IOException {
+        if (depth == M) {
+            for (int n : numbers) bw.write(n + " ");
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = root; i < N; i++) {
+            numbers[depth] = inputArr[i];
+            dfs(depth + 1, i + 1);
+        }
+    }
+}
